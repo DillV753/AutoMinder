a3e1a207f019ae87469c830a8163e29a942470d5
Dillon Vu <ddvu8@cougarnet.uh.edu>
2026-06-24 18:36:13 -0500

    Fix client folder tracking

---
+++ client/src/Login.js
@@ -0,0 +1,12 @@
+function Login() {
+  return (
+    <div>
+      <h2>Login to AutoMinder</h2>
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button>Log In</button>
+    </div>
+  );
+}
+
+export default Login;
