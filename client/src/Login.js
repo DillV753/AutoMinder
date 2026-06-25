403876f54393e75ad892fc09f391d70b16038d70
Dillon Vu <ddvu8@cougarnet.uh.edu>
2026-06-24 19:54:56 -0500

    Added Logo

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,9 +1,16 @@
 function Login() {
   return (
-    <div>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100vh'
+    }}>
+      <img src="/AutoMinderLogo.png" alt="AutoMinder Logo" style={{ width: '250px', marginBottom: '5px' }} />
       <h2>Login to AutoMinder</h2>
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
+      <input type="email" placeholder="Email" style={{ marginBottom: '10px' }} />
+      <input type="password" placeholder="Password" style={{ marginBottom: '10px' }} />
       <button>Log In</button>
     </div>
   );
