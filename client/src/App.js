dd1628654020a589ecb2bab6d01ca52ceab0e4d8
Dillon Vu <ddvu8@cougarnet.uh.edu>
2026-06-25 20:43:08 -0500

    Added Sign Up page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+/*import './App.css';
 import Login from './Login';
 
 function App() {
   return (
     <div className="App">
       <Login />
+    </div>
+  );
+}
+
+export default App;*/
+
+import './App.css';
+import SignUp from './SignUp';
+
+function App() {
+  return (
+    <div className="App">
+      <SignUp />
     </div>
   );
 }
---
+++ client/src/SignUp.js
@@ -0,0 +1,20 @@
+function SignUp() {
+  return (
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100vh'
+    }}>
+      <img src="/AutoMinderLogo.png" alt="AutoMinder Logo" style={{ width: '120px', marginBottom: '10px', marginTop: '15px' }} />
+      <h2>Create your AutoMinder account</h2>
+      <input type="text" placeholder="Full Name" style={{ marginBottom: '10px' }} />
+      <input type="email" placeholder="Email" style={{ marginBottom: '10px' }} />
+      <input type="password" placeholder="Password" style={{ marginBottom: '10px' }} />
+      <button>Create Account</button>
+    </div>
+  );
+}
+
+export default SignUp;
